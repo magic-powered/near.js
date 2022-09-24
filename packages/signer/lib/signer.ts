e1b81f93da81602877af87573a34f9db100a3b4b
Anatolii Petrovskyi <anatolii@magicpowered.io>
2022-09-24 19:44:44 +0300

    added TODO for later dev

--- packages/signer/lib/signer.ts
+++ packages/signer/lib/signer.ts
@@ -2,7 +2,9 @@ import { ITransaction } from '@near.js/tx';
 import { KeyPair } from '@near.js/keys';
 import { ISignedTransaction } from "./signed_transaction";
 
-// TODO init with specific key pair
+// TODO: sign any (string)
+// TODO: verify tx signature
+// TODO: verify any signature
 
 export class Signer {
   private readonly keyPair: KeyPair;
