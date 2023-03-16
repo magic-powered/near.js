a1ca6f51f7c5bba3dcd234418512577a46eced09
Anatolii Petrovskyi <anatolii@magicpowered.io>
2023-03-16 22:41:27 +0200

    extend provider-my-near-wallet exports

--- packages/provider-wallet-my-near-wallet/lib/index.ts
+++ packages/provider-wallet-my-near-wallet/lib/index.ts
@@ -1,3 +1,5 @@
 export * from './my-near-wallet-configuration';
 export * from './my-near-wallet-sign-in-options';
 export * from './provider-my-near-wallet';
+export * from '@nearjs/provider-core';
+export { IAction } from '@nearjs/tx';
