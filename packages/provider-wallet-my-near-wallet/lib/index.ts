22577fd1d236aa6fe4d3947fcd994be14e8e83dd
Anatolii Petrovskyi <anatolii@magicpowered.io>
2023-03-16 22:49:57 +0200

    fix export structure

--- packages/provider-wallet-my-near-wallet/lib/index.ts
+++ packages/provider-wallet-my-near-wallet/lib/index.ts
@@ -1,5 +1,3 @@
 export * from './my-near-wallet-configuration';
 export * from './my-near-wallet-sign-in-options';
 export * from './provider-my-near-wallet';
-export * from '@nearjs/provider-core';
-export { IAction } from '@nearjs/tx';
