a06d3ca44a9ddad5e2b4222c745071633d596304
Anatolii Petrovskyi <anatolii@magicpowered.io>
2023-03-20 17:07:42 +0200

    cleanup

--- packages/web-sandbox/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  plugins: [react()],
-});
